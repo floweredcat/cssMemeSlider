32a7a5e0fe152cc3062de898b739d4fcbc318572
Danil Bugaenko <danil.bugaenko@mail.ru>
2022-05-13 09:44:13 +0300

    feat: adaptive css

---
+++ cssMemeSlider/scripts/script.js
@@ -0,0 +1,22 @@
+const memesData = [
+    {
+        name: "SovaOrCamera",
+        picSelector: "",
+        captionSelector: ""
+    },
+    {
+        name: "SovaOrCamera",
+        picSelector: "toSecondPic",
+        captionSelector: "toSecondCaption"
+    },
+    {
+        name: "SovaOrCamera",
+        picSelector: "toThirdPic",
+        captionSelector: "toThirdPicCaption"
+    },
+    {
+        name: "SovaOrCamera",
+        picSelector: "tofourthPic",
+        captionSelector: "tofourthPicCaption"
+    }
+]
